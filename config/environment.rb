0ae6f7c6e38388429f4d3327bdbbda91402d110f
linurit <eolson@operations.net>
2017-03-19 06:32:05 -0700

    Commit#

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# Rails.logger = Le.new('becd7d88-e48a-4bae-87e9-9ddcc0cce9d7', :debug => true, :local => true)
-
 # Initialize the Rails application.
 Rails.application.initialize!
-
-
-# Rails.logger.warn("Look at me, I'm a warning message")
-# Rails.logger.info("Look at me, I'm an info message")
-# Rails.logger.debug("Look at me, I'm a debug message")
