9f8961e9e0ea984c6cadee451305ca80b2e9e81e
linurit <eolson@operations.net>
2017-03-19 06:59:58 -0700

    der

--- app/models/cyber_insult.rb
+++ app/models/cyber_insult.rb
@@ -45,7 +45,7 @@ client = Twitter::REST::Client.new do |config|
   config.consumer_secret     = ENV["CONSUMER_SECRET"]
   config.access_token        = ENV["ACCESS_TOKEN"]
   config.access_token_secret = ENV["ACCESS_SECRET"]
-
+end
 
 insult = CyberInsult.new
 client.update(insult.print_insult)
