80cbef14ed50e67f20e51f83165b52d75c578f03
linurit <eolson@operations.net>
2017-03-24 21:16:03 -0700

    blah

--- app/models/cyber_insult.rb
+++ app/models/cyber_insult.rb
@@ -8,7 +8,8 @@ class CyberInsult
 
   PRONOUNS = ["She", "He", "That bastard", "That bitch", "Neo", "Acid Burn", "The Plague",
                 "J Random Hacker", "That hacker", "That console cowboy",
-                "That decker", "The boss", "My boss", "The new kid", "That kid" ]
+                "That decker", "The boss", "My boss", "The new kid", "That kid",
+                "the cyber czar" ]
 
 
   COMPARISON = ["looks like a", "looked like a", "smells like a", "smelled like a",
@@ -17,28 +18,32 @@ class CyberInsult
                 "is a", "was a", "has eyes like a", "played like a", "plays like a",
                 "moves like a", "moved like a", "dresses like a", "walks like a",
                 "walked like a", "talked like a", "talks like a", "died like a",
-                "acts like a", "acted like a" ]
+                "acts like a", "acted like a", "has the dead eyes of"]
 
   NOUNS = ["20th century tourist", "wilson", "noob", "fed", "random", "bot",
                 "warez d00d", "l33t d00d", "flatline", "raver", "tourist",
                 "buttonhead", "mall ninja", "cyber-criminal", "hacker", "poser",
                 "dot-matrix printer", "algorithm", "blogger", "skid", "virus",
                 "pirate", "COBOL programmer", "C developer", "perl addict",
-                "Apple fanatic" ]
+                "Apple fanatic", "Atari fanatic", "Amiga fanatic", "a chump",
+                "a cyber-tourist" ]
 
   CONDITIONS = ["with no skillz", "who just got beat", "three days dead",
                 "with broken hands", "on DSL", "on his grampa's modem",
                 "on rollerblades", "on designer drugs", "on ISDN",
                 "in  mirrorshades", "from the Sprawl", "on derms",
                 "full of pills", "in cyberspace", "in my mentions",
-                "sliding into my DMs", "at Defcon", "at B-Sides", "at RSA" ]
+                "sliding into my DMs", "at Defcon", "at B-Sides", "at RSA",
+                "on an Amiga", "on an Atari", "on a Ono-Sendai", "on a Apple ][e",
+                "in heat"]
+                 ]
 
 
   def initialize
     @fl_pronoun = PRONOUNS.sample
     @fl_noun = NOUNS.sample
     @fl_comp = COMPARISON.sample
-    @fl_cond = CONDITIONS.sample
+    @fl_cond = CONDITIONS.sample(2)
 	end
 
 
